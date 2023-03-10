0ba8183bb6ba69783e2ea3e9a7d500d7366a7d35
kps9401 <kps9401@naver.com>
2023-03-10 16:10:43 +0900

    vm instance model has been changed

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "google_compute_network" "vpc_network" {
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
-  machine_type = "f1-micro"
+  machine_type = "n2-standard-2"
   tags		   = ["web", "dev"]
 
   boot_disk {
