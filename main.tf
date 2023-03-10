d85ca01adff721bd3a93bd060bbc01a1f4e407b6
kps9401 <kps9401@naver.com>
2023-03-10 15:25:22 +0900

    fist commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("global-pagoda-379200-223d239b4974.json")
+
+  project = "global-pagoda-379200"
+  region  = "asia-northeast3"
+  zone    = "asia-northeast3-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags		   = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
